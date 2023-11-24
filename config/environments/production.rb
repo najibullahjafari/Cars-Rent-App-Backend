c968e4dffca99712dae9806d5fd66127c852fc34
Najibullahjafari <najib2020202020@gmail.com>
2023-11-24 07:33:41 +0430

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
   onfig.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
+  config.assets.compile = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
