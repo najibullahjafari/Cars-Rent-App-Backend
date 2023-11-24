2dc12230acdc3c3195e3022cf3aa3b4136d15597
Najibullahjafari <najib2020202020@gmail.com>
2023-11-24 09:00:23 +0430

    Update

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,6 @@ module CarsRentAppBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     # config.api_only = true
-    config.assets.enabled = true
+    # config.assets.enabled = true
   end
 end
