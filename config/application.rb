f7bc5b3b2f56b0ad2743e41fc72a7adc1f7935fc
Najibullahjafari <najib2020202020@gmail.com>
2023-11-24 08:53:23 +0430

    remove require srpockets

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require 'action_mailbox/engine'
 require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
-require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -43,6 +42,5 @@ module CarsRentAppBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     # config.api_only = true
-    config.assets.enabled = true
   end
 end
