9d6d4b48269270503da83017cfc28214f9301dde
Najibullahjafari <najib2020202020@gmail.com>
2023-11-24 16:59:40 +0430

    U[date

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module CarsRentAppBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    Bundler.require(:default, :assets, Rails.env)
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
