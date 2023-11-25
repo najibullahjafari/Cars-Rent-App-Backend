84713b87d08e79f4c08204da04e4b566017b1d49
Najibullahjafari <najib2020202020@gmail.com>
2023-11-25 18:53:50 +0430

    Update

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module CarsRentAppBackend
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.autoloader = :classic
+    config.eager_load = true
+
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
